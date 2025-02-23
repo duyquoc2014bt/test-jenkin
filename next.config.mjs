bb3f08e5b98e699301cc4acd6d8b4c0f9237e4be
duyquoc2014bt <duyquoc2014bt@gmail.com>
2025-02-23 09:44:12 +0700

    feat: change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: "standalone",
+  images: {
+    unoptimized: true, // Giúp tránh lỗi mất ảnh khi build tĩnh
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/_next/" : "",
 };
 
 export default nextConfig;
